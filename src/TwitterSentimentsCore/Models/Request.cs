858817459351365d99480a5d8698edf160f48a48
James Earle <j_earle@hotmail.com>
2016-08-23 12:02:30 -0700

    unstaged changes?

--- src/TwitterSentimentsCore/Models/Request.cs
+++ src/TwitterSentimentsCore/Models/Request.cs
@@ -10,6 +10,7 @@ namespace TwitterSentimentsCore.Models
     // TODO: sorting on count and results, maybe alphanumeric on twitter handle too
     // TODO: check out CoreTweet to see if there are other cool things it can do
     // TODO: update so instead of inserting requested count, it inserts the actual amount analyzed
+    // TODO: search stuff?
 
     public class Request
     {
